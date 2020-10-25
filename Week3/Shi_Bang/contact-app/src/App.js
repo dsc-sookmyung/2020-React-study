44d232f8b6c7f4ac2de39827478d3b4883dd0e99
siraegi <shihabbong11@gmail.com>
2020-10-25 22:59:49 +0900

    Add PhoneInfo, PhoneInfoList

--- Week3/Shi_Bang/contact-app/src/App.js
+++ Week3/Shi_Bang/contact-app/src/App.js
@@ -1,14 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
-function App() {
-  return (
-    <div>
-      <PhoneForm/>
-    </div>
-  );
+class App extends Component {
+
+  id = 3;
+
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      },
+      {
+        id: 1,
+        name: '시래기',
+        phone: '010-1234-5678'
+      },
+      {
+        id: 2,
+        name: '방소융천사',
+        phone: '010-9876-5432'
+      }
+    ],
+    keyword: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      keyword: e.target.value,
+    })
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat(Object.assign({}, data, {
+        id: this.id++
+      }))
+    });
+  }
+
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    });
+  }
+
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info =>{
+          if (info.id === id) {
+            return {
+              id,
+              ...data,
+            }
+          }
+          return info;
+        })
+    });
+  }
+  render() {
+      return (
+        <div>
+          <PhoneForm onCreate={this.handleCreate}/>
+          <input 
+            value={this.state.keyword}
+            onChange={this.handleChange}
+            placeholder="검색..."
+          />
+          <PhoneInfoList
+            data={this.state.information.filter(
+              info => info.name.indexOf(this.state.keyword) > -1
+            )}
+            onRemove={this.handleRemove}
+            onUpdate={this.handleUpdate}/>
+        </div>
+      );
+  };
 }
 
 export default App;
--- Week3/Shi_Bang/contact-app/src/components/PhoneForm.js
+++ Week3/Shi_Bang/contact-app/src/components/PhoneForm.js
@@ -1,37 +1,47 @@
 import React, { Component } from 'react';
 
 class PhoneForm extends Component {
+    input = React.createRef();
+
     state = {
         name: '',
         phone: '',
     }
 
-    handleTargets = (e) => {
+    handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
         });
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onCreate(this.state);
+        this.setState({
+            name: '',
+            phone: ''
+        });
+        this.input.current.focus();
+    }
+
     render() {
         return (
-            <div>
+            <form onSubmit={this.handleSubmit}>
                 <input 
                     name="name" 
                     placeholder="이름" 
-                    onChange={this.handleTargets} 
+                    onChange={this.handleChange} 
                     value={this.state.name}
+                    ref={this.input}
                 />
                 <input 
                     name="phone" 
                     placeholder="전화번호" 
-                    onChange={this.handleTargets} 
+                    onChange={this.handleChange} 
                     value={this.state.phone}
                 />
-                <div>
-                    {this.state.name}
-                    {this.state.phone}
-                </div>
-            </div>
+                <button type="submit">등록</button>
+            </form>
         );
     }
 }
