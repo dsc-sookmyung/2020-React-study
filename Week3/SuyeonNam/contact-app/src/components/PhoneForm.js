f521839a48691c63fa99ecaef74c37ddac989006
mori8 <kaithape@gmail.com>
2020-10-31 23:03:19 +0900

    add week4 assignments

--- Week3/SuyeonNam/contact-app/src/components/PhoneForm.js
+++ Week3/SuyeonNam/contact-app/src/components/PhoneForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 class PhoneForm extends Component {
+    input = React.createRef; // Ref를 통해 해당 DOM에 대한 접근 가능
     state = {
         name: '',
         phone: '',
@@ -12,15 +13,33 @@ class PhoneForm extends Component {
         });
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault(); // 페이지 새로고침 방지
+        this.props.onCreate({
+            name: this.state.name,
+            phone: this.state.phone,
+        })
+        this.setState({
+            name: '',
+            phone: '',
+        })
+        this.input.current.focus(); // ref를 통해 focus
+    }
+
     render() {
         return (
-            <form>
-                <input name ="name" placeholder="이름" onChange={this.handleChange} value={this.state.name}/>
-                <input name="phone" placeholder="전화번호" onChange={this.handleChange} value={this.state.phone}/>
-                <div>
-                    {this.state.name}
-                    {this.state.phone}
-                </div>
+            <form onSubmit={this.handleSubmit}> 
+                <input
+                    name ="name"
+                    placeholder="이름"
+                    onChange={this.handleChange}
+                    value={this.state.name}
+                    ref={this.input}/> {/* 여기로 focus */}
+                <input name="phone"
+                    placeholder="전화번호"
+                    onChange={this.handleChange}
+                    value={this.state.phone}/>
+                <button type="submit">등록</button>
             </form>
         );
     }
