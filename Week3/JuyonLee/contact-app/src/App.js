08da115abbf8a7eb336f9cb378e6a840dbb29b82
LemonYellowParrot <ljy012900@gmail.com>
2020-11-01 16:12:56 +0900

    Week3 assignment

---
+++ Week3/JuyonLee/contact-app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import PhoneForm from './components/PhoneForm'
+
+function App() {
+  return (
+    <div className="App">
+      <PhoneForm />
+    </div>
+  );
+}
+
+export default App;
