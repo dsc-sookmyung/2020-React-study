ebbb5192957bef4dae044bb2ce5bfe481a6ddb8c
hyeju1123 <hjjun1123@gmail.com>
2020-11-02 10:30:27 +0900

    add week4 assignment

--- Week3/HyeJuJun/contact-app/src/components/PhoneForm.js
+++ Week3/HyeJuJun/contact-app/src/components/PhoneForm.js
@@ -10,9 +10,17 @@ class PhoneForm extends Component {
             [e.target.name]: e.target.value,
         });
     }
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onCreate(this.state);
+        this.setState({
+            name: '',
+            phone: ''
+        })
+    }
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <input 
                     name="name"
                     placeholder="이름" 
@@ -25,10 +33,7 @@ class PhoneForm extends Component {
                     onChange={this.handleChange}
                     value={this.state.phone} 
                 />
-                <div>
-                    {this.state.name}
-                    {this.state.phone}
-                </div>      
+                <button type="submit">등록</button>
             </form>
         );
     }
